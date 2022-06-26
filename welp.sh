01d00af08ebcff7dfa27b46dbbdb21ea4a4366cb
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-06-26 11:39:15 +0300

    (Unfinished) script

---
+++ welp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Made by RaptaG cuz why not :)
+
+# Crash Checker
+set -e
+
+# Definitions
+ver=1.0
+
+# Script info message
+echo 'welp, version $ver (by RaptaG)'
+
+# Main script
+main() {
+    # Which command you want to search
+    echo -n 'Enter the command you want to search: '
+    read answer
+}
